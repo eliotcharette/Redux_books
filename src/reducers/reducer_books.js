260f6d5405b23fc6d28ae46e96e87482d5e7f61a
Eliot Charette <eliotcharette@gmail.com>
2018-09-23 09:58:15 -0700

    Incorporate activebook reducer, finish app

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'The Alchemist' },
-    { title: 'Lord of the Rings' },
-    { title: 'Harry Potter' },
-    { title: 'Cracking the Code' },
+    { title: 'The Alchemist', pages: 101 },
+    { title: 'Lord of the Rings', pages: 56 },
+    { title: 'Harry Potter', pages: 67 },
+    { title: 'Cracking the Code', pages: 100 },
   ]
 }
